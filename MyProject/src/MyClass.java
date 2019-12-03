39e7eaf4f402a415107e8d87c94b90939274aa72
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:09 +0530

    this is my testdemo project

---
+++ MyProject/src/MyClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	System.out.println("hey hello ");
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		int b=s.next();
+ System.out.println("hello"+a);
+ System.out.println("age"+a);
+
+	}
+
+}
